17e74f12e9fbd6bc17f491ce038035d2fb7bb230
Michael Peralta <peralta.michael27@gmail.com>
2024-03-20 03:41:19 -0400

    Create Tkinter UI and adding API data

--- ui.py
+++ ui.py
@@ -1,2 +1,64 @@
+from tkinter import *
+from quiz_brain import QuizBrain
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+        self.window.minsize(width=350, height=500)
+
+        self.score_text = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_text.grid(row=0, column=1)
+
+        self.canvas = Canvas(height=250, width=300, background="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="question text",
+            font=("Arial", 20, "italic"),
+            fill=THEME_COLOR
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        check_image = PhotoImage(file="images/true.png")
+        self.check_button = Button(image=check_image, command=self.true_pressed)
+        self.check_button.grid(row=2, column=0)
+        cross_image = PhotoImage(file="images/false.png")
+        self.cross_button = Button(image=cross_image, command=self.false_pressed)
+        self.cross_button.grid(row=2, column=1)
+
+        self.get_next_question()
+        self.window.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg='white')
+        if self.quiz.still_has_questions():
+            self.score_text.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="No more questions")
+            self.check_button.config(state=DISABLED)
+            self.cross_button.config(state=DISABLED)
+
+    def true_pressed(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def false_pressed(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right: bool):
+
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg='red')
+        self.window.after(1000, func=self.get_next_question)
